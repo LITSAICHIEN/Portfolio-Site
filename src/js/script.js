02c2a7fe44ecf5f1acd761a01d387e5edce83cbe
LITSAICHIEN <39357707+LITSAICHIEN@users.noreply.github.com>
2018-07-11 16:51:53 +0900

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -74,8 +74,18 @@ $(document).ready(function(){
     });
 });
 
-//ここからはスムーススクロール
 $(document).ready(function(){
+  $('a[href^=#]').click(function(){
+    var speed = 800;
+    var href=$(this).attr("href");
+    var target=$(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
+$(document).ready(function(){
+  //ここからはスムーススクロール
   $('#top').click(function() {
     $('html, body').animate({
       'scrollTop':0
